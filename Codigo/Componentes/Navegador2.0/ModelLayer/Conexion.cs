d7b102aa0385625afbc83af9e0b357825a19bd39
D4nnny <dperezf5@miumg.edu.gt>
2024-06-06 10:23:31 -0600

    Agregnado Menu HCV, Ejecucion y Script de base de datos

---
+++ Codigo/Componentes/Navegador2.0/ModelLayer/Conexion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace ModelLayer
+{
+    public class Conexion
+    {
+        public OdbcConnection connection()
+        {
+            OdbcConnection conn = new OdbcConnection("Dsn=HotelSConexion");
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+            return conn;
+        }
+
+
+        public void disconnect(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+        }
+    }
+}
